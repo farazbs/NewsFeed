b6c018130b7eeba7fa73ab71c8b343ae8a85e768
FAraz <faraz.zafar@emaratech.ae>
2018-11-23 18:41:34 +0400

    - Setting up TableView design for listing News feed

---
+++ NewsFeed/NewsFeed/Cell/CellNewsFeed.swift
@@ -0,0 +1,16 @@
+//
+//  CellNewsFeed.swift
+//  NewsFeed
+//
+//  Created by admin on 11/23/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class CellNewsFeed: UITableViewCell {
+    @IBOutlet weak var lblPublishedAt : UILabel!
+    @IBOutlet weak var lblTitle : UILabel!
+    @IBOutlet weak var lblAuthor : UILabel!
+    @IBOutlet weak var imgview : UIImageView!
+}
--- NewsFeed/NewsFeed/Controllers/ViewController.swift
+++ NewsFeed/NewsFeed/Controllers/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableviewNewsFeed : UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
