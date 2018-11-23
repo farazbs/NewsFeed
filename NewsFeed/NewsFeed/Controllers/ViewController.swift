116af6914e7fb995d94f19ced8f7fe925544850d
FAraz <faraz.zafar@emaratech.ae>
2018-11-23 19:40:42 +0400

    - Load Data in TableView and Add SDWebImage library(It loads data once only till application life cycle)

--- NewsFeed/NewsFeed/Controllers/ViewController.swift
+++ NewsFeed/NewsFeed/Controllers/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import ObjectMapper
+import SDWebImage
 
 class ViewController: UIViewController {
     
@@ -35,10 +36,35 @@ class ViewController: UIViewController {
                 
                 // Feeding newsFeed model object with data
                 self.newsFeed = Mapper<NewsFeed>().map(JSON: JSONResponse.dictionaryObject!)
+                self.tableviewNewsFeed.reloadData()
             }
         }
     }
-
-
+    
+}
+extension ViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 82
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.newsFeed == nil || self.newsFeed.articles == nil{
+            return 0
+        }else{
+            return (self.newsFeed.articles?.count)!
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellNewsFeed = tableviewNewsFeed.dequeueReusableCell(withIdentifier: "CellNewsFeed") as! CellNewsFeed
+        let article = self.newsFeed.articles![indexPath.row]
+        cellNewsFeed.lblPublishedAt.text = article.publishedAt
+        cellNewsFeed.lblTitle.text = article.title
+        cellNewsFeed.lblAuthor.text = article.author
+        // SD image load data once only and it cache till the application life cycle
+        cellNewsFeed.imgview.sd_setImage(with: URL(string: article.urlToImage!), placeholderImage: UIImage(named: "img_placeholder"), options: [], completed: nil)
+        return cellNewsFeed
+    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
 }
 
